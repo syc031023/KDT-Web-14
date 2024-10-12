0f8d36b922b40baf4eded14d0b2195e026eaf7fc
syc031023 <syc031023@naver.com>
2024-10-12 09:09:45 +0900

    실습

--- 09-node.js/09-3_express/app.js
+++ 09-node.js/09-3_express/app.js
@@ -7,6 +7,12 @@ app.set("view engine", "ejs");
 // 템플릿 엔진 파일을 저장할 위치 등록 (default 폴더명 views)
 app.set("views", "./views");
 
+// static 미들웨어 등록 (정적 파일 로드 ex. css, js, img, ...)
+// static 이라는 실제 폴더를 public 이라는 이름으로 접근하겠다. 
+app.use("/public", express.static(__dirname + "/static")); //상대경로로 해도 가능
+console.log(__dirname); // ~~~/KDT-Web-14/09-node.js/09-3_express
+
+
 // app.get(경로, 해당 경로로 들어왔을 떄 실행할 함수)
 // 요청 메서드: get
 // 요청 주소: "/" -> "서버 주소:포트번호/" -> "http://localhost:8000"
